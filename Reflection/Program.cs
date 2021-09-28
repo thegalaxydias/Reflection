a627216bdaa1228742c9099da2d9a3dcb6431ab4
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-28 22:14:38 +0300

    Добавьте файлы проекта.

---
+++ Reflection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Type a = typeof(String);           
+            PropertyInfo[] prop = a.GetProperties();
+            foreach (var p in prop)
+            {
+            Console.WriteLine(p);
+            }
+
+
+        }
+    }
+}
